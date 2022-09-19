67e9b0f2303425cdb56bdcfc67e5a5276ec51c34
rewks <rewks@users.noreply.github.com>
2022-09-19 18:27:15 +0100

    day 25!

--- 2015/day25/src/main.rs
+++ 2015/day25/src/main.rs
@@ -1,6 +1,23 @@
+
+fn get_n(x: u64, y: u64) -> u64 {
+    (0..=(x + y - 1)).sum::<u64>() - (y - 1)
+}
+
+fn get_code(previous_code: u64) -> u64 {
+    (previous_code * 252533) % 33554393
+}
+
 fn main() {
     let data = include_str!("../input.txt");
-    println!("Part 1: {}", "");
+    let tokens: Vec<&str> = data.split(" ").collect();
+    let y: u64 = tokens[16].trim_end_matches(",").parse().unwrap();
+    let x: u64 = tokens[18].trim_end_matches(".\n").parse().unwrap();
 
-    println!("Part 2: {}", "");
+    let n = get_n(x, y);
+    let mut code = 20151125;
+    for _i in 2..=n {
+        code = get_code(code);
+    }
+
+    println!("Secret Code: {}", code);
 }
